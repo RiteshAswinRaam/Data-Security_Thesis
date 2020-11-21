b627604981737868a11c9aaa77f2af1cd7d8fce6
RiteshAswin <riteshaswin123@gmail.com>
2020-11-21 18:04:37 +0000

    First commit

---
+++ Admin/Upload_File.aspx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Net.Mail;
+
+
+
+public partial class Upload_File : System.Web.UI.Page
+{
+    public int i, j, k;
+    public static string cs = ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString;
+    public SqlConnection conn;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["a_id"] == "")
+        {
+            Response.Redirect("Login.aspx?msg=logout");
+
+        }
+    }
+    protected void btnSave_Click(object sender, EventArgs e)
+    {
+        if (FileUpload1.HasFile)
+        {
+
+            string fileName = Path.GetFileNameWithoutExtension(FileUpload1.PostedFile.FileName);
+            string fileExtension = Path.GetExtension(FileUpload1.PostedFile.FileName);
+            string file_path = Path.GetFileName(FileUpload1.PostedFile.FileName);
+           
+
+
+                //Build the File Path for the original (input) and the encrypted (output) file.
+                string input = Server.MapPath("../Files/") + fileName + fileExtension;
+                string file_name1 = fileName + "_enc1" + fileExtension;
+                //string file_name2 = fileName + "_enc2" + fileExtension;
+                //string file_name3 = fileName + "_enc3" + fileExtension;
+                //string output1 = Server.MapPath("../Files/" + file_name1);
+                //string output2 = Server.MapPath("../Files/" + file_name2);
+                //string output3 = Server.MapPath("../Files/" + file_name3);
+
+                string output = Server.MapPath("../Files/") + fileName + "_enc1" + fileExtension;
+                //Save the Input File, Encrypt it and save the encrypted file in output path.
+                FileUpload1.SaveAs(input);
+
+                SqlDataAdapter _adp_R = new SqlDataAdapter("select Top(1)* from auto_keys order by NEWID()", Database.cs);
+                DataTable _dtr = new DataTable();
+                _adp_R.Fill(_dtr);
+                string encryptedkey;
+                if (_dtr.Rows.Count > 0)
+                {
+                    encryptedkey = _dtr.Rows[0]["key_value"].ToString();
+                }
+                else
+                {
+                    encryptedkey = "0";
+                }
+
+                string key1 = encryptedkey;
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(key1);
+
+                Stream inputStream = FileUpload1.PostedFile.InputStream;
+                Byte[] data;
+                using (var streamReader = new MemoryStream())
+                {
+                    inputStream.CopyTo(streamReader);
+                    data = streamReader.ToArray();
+                }
+
+
+                Encrypt(key1, input,output);
+
+            conn=new SqlConnection(cs);
+            using (
+                SqlCommand cmd =
+                    new SqlCommand(
+                        "insert into file_master (file_path,key_value,file_name) values(@file_path,@key_value,@file_name)",
+                        conn))
+            {
+                cmd.Parameters.AddWithValue("@file_name", file_path);
+                cmd.Parameters.AddWithValue("@file_path", file_name1);
+                cmd.Parameters.AddWithValue("@key_value", key1);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+
+                //string query = "Insert Into File_Info values('" + file_path + "','" + encryptedkey+"')";
+                //Database.InsertData_direct(query);
+                Response.Redirect("Manage Files.aspx?msg=add");
+
+            }
+
+        }
+  
+    private void Encrypt(string key, string inputFilePath, string outputfilePath)
+    {
+        string EncryptionKey = key;
+        using (Aes encryptor = Aes.Create())
+        {
+            Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+            encryptor.Key = pdb.GetBytes(32);
+            encryptor.IV = pdb.GetBytes(16);
+            using (FileStream fsOutput = new FileStream(outputfilePath, FileMode.Create))
+            {
+                using (CryptoStream cs = new CryptoStream(fsOutput, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    using (FileStream fsInput = new FileStream(inputFilePath, FileMode.Open))
+                    {
+                        int data;
+                        while ((data = fsInput.ReadByte()) != -1)
+                        {
+                            cs.WriteByte((byte)data);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ Users/Download_File.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Configuration;
+using System.Net;
+
+public partial class Users_Download_File : System.Web.UI.Page
+{
+    public string file_path, key, fileName, fileExtension, output, input_file;
+    public static string cs = ConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString;
+    public SqlConnection conn;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["user_id"] == "")
+        {
+            Response.Redirect("Login.aspx?msg=logout");
+        }
+    }
+
+    protected void btnSave_Click(object sender, EventArgs e)
+    {
+         conn=new SqlConnection(cs);
+        string share_id = Request.QueryString["share_id"];
+        string query = "select file_name,fid from Share_Master where share_id='" + share_id + "'";
+        DataTable dt = Database.Getdata(query);
+
+        string file_name = Convert.ToString(dt.Rows[0]["file_name"]);
+        int fileid = Convert.ToInt32(dt.Rows[0]["fid"]);
+
+        using (SqlCommand cmd = new SqlCommand("select * from file_master where key_value=@key_value", conn))
+        {
+            cmd.Parameters.AddWithValue("@key_value", txtsecret_key.Text);
+            using (SqlDataAdapter adp = new SqlDataAdapter(cmd))
+            {
+                DataTable dt1 = new DataTable();
+                adp.Fill(dt1);
+                if (dt1.Rows.Count > 0)
+                {
+                    file_path = Convert.ToString(dt1.Rows[0]["file_path"]);
+                    fileName = Path.GetFileNameWithoutExtension(file_path);
+                    fileExtension = Path.GetExtension(file_path);
+                    output = Server.MapPath("../Files/"+fileName + "_dec" + fileExtension);
+                    input_file = Server.MapPath("../Files/" + file_path);
+                    key = txtsecret_key.Text;
+                    Decrypt(key, input_file, output);
+                    WebClient req = new WebClient();
+                    HttpResponse response = HttpContext.Current.Response;
+                    string filePath = "~/Files/" + fileName + "_dec" + fileExtension;
+                    response.Clear();
+                    response.ClearContent();
+                    response.ClearHeaders();
+                    response.Buffer = true;
+                    response.AddHeader("Content-Disposition", "attachment;filename=" + dt1.Rows[0]["file_name"].ToString());
+                    byte[] data = req.DownloadData(Server.MapPath(filePath));
+                    response.BinaryWrite(data);
+                    response.End();
+                }
+            }
+        }
+
+
+
+    }
+
+    private void Decrypt(string key, string inputFilePath, string outputfilePath)
+            {
+                string EncryptionKey = key;
+                using (Aes encryptor = Aes.Create())
+                {
+                    Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                    encryptor.Key = pdb.GetBytes(32);
+                    encryptor.IV = pdb.GetBytes(16);
+                    using (FileStream fsInput = new FileStream(inputFilePath, FileMode.Open))
+                    {
+                        using (CryptoStream cs = new CryptoStream(fsInput, encryptor.CreateDecryptor(), CryptoStreamMode.Read))
+                        {
+                            using (FileStream fsOutput = new FileStream(outputfilePath, FileMode.Create))
+                            {
+                                int data;
+                                while ((data = cs.ReadByte()) != -1)
+                                {
+                                    fsOutput.WriteByte((byte)data);
+
+                                    
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+}
